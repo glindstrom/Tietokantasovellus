a6d69fddb5aa5abe79104e2878ae04c3c031bd59
Gabriel Lindstrom <gabriel.lindstrom@gmail.com>
2013-08-28 10:46:45 +0300

    Updated createtables.sql

--- sql/createtables.sql
+++ sql/createtables.sql
@@ -6,37 +6,37 @@ CREATE TABLE users(
 
 CREATE TABLE league(
 	id serial PRIMARY KEY,
-	name varchar NOT NULL
+	name varchar UNIQUE NOT NULL
 );
 
 CREATE TABLE season(
 	id serial PRIMARY KEY,
-	name varchar NOT NULL
+	name varchar UNIQUE NOT NULL
 );
 
 CREATE TABLE team(
 	id serial PRIMARY KEY,
-	name varchar NOT NULL
+	name varchar UNIQUE NOT NULL
 );
 
 CREATE TABLE membership(
 	id serial PRIMARY KEY,
-	league_id integer references league(id) ON DELETE CASCADE,
-	season_id integer references season(id) ON DELETE CASCADE,
-	team_id integer references team(id) ON DELETE CASCADE
+	league_id integer NOT NULL REFERENCES league(id) ON DELETE CASCADE,
+	season_id integer NOT NULL REFERENCES season(id) ON DELETE CASCADE,
+	team_id integer REFERENCES team(id) ON DELETE CASCADE
 );
 
 CREATE TABLE game(
 	id serial PRIMARY KEY,
-	home_team integer references team(id) ON DELETE CASCADE,
-    away_team integer references team(id) ON DELETE CASCADE,
-	league_id int references league(id) ON DELETE CASCADE,
-    season_id int references season(id) ON DELETE CASCADE,
+	home_team integer NOT NULL REFERENCES team(id) ON DELETE CASCADE,
+    away_team integer NOT NULL REFERENCES team(id) ON DELETE CASCADE,
+	league_id integer NOT NULL REFERENCES league(id) ON DELETE CASCADE,
+    season_id integer NOT NULL REFERENCES season(id) ON DELETE CASCADE,
 	home_score integer,
 	away_score integer,
 	game_date date NOT NULL,
 	time_edited timestamp,
-	edited_by integer references users(id) ON DELETE RESTRICT
+	edited_by integer REFERENCES users(id) ON DELETE RESTRICT
 );
 
 	
